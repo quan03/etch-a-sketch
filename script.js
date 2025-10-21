24a255c13b75ee81c5fe9c2128dc13266d30c74d
Quan Nguyen <quan.ng321@gmail.com>
2025-10-21 15:12:34 +1100

    added fixed 16x16 frid and pen capability

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // Add your JavaScript code here
 console.log('JavaScript file loaded successfully');
+
+const container = document.querySelector(".flex-container");
+
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement("div");
+    container.append(div);
+}
+
+container.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = 'blue';
+    });
